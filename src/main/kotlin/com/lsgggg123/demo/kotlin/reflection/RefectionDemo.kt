ee8f2282041e20b46a44416755d5a2182332c5e8
刘顺 <lsgggg123@gmail.com>
2021-02-14 00:00:18 +0800

    reflection demo

--- src/main/kotlin/com/lsgggg123/demo/kotlin/reflection/RefectionDemo.kt
+++ src/main/kotlin/com/lsgggg123/demo/kotlin/reflection/RefectionDemo.kt
@@ -1,22 +1,28 @@
 package com.lsgggg123.demo.kotlin.reflection
 
 import kotlin.reflect.KMutableProperty
-import kotlin.reflect.full.functions
-import kotlin.reflect.full.memberFunctions
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.full.superclasses
+import kotlin.reflect.full.*
 
 /*
     p45
  */
 class TypeParameterGeneric<K, V>(var k: K, var v: V)
-class TypeParameter(var k: Int, var v: String) {
+class TypeParameter(var k: Int = 1, var v: String = "a") {
+
+    // constructor(): this(3, "c")
+
     fun printInfo() {
         println("k is $k, v is $v")
     }
 
     fun printInfoWithParam(x: String) {
         println("k is $k, v is $v, param is $x")
+    }
+
+    companion object {
+        fun companionMethod() {
+            println("hello world")
+        }
     }
 }
 
@@ -27,15 +33,18 @@ fun main() {
     println(typeParameterGenericClz.typeParameters[0])
     println(typeParameterGenericClz.typeParameters[1])
     println(typeParameterGenericClz.superclasses)
+    println("------------------------------------")
 
-    val typeParameter = TypeParameter(1, "a")
+    val typeParameter = TypeParameter(2, "b")
+    val kClass = TypeParameter::class
     println(typeParameter::class.memberProperties)
 
-    TypeParameter::class.memberFunctions.filter { it.name == "printInfo" }.forEach { it.call(typeParameter) }
-    TypeParameter::class.functions.find { it.name == "printInfo" }?.call(typeParameter)
-    TypeParameter::class.functions.find { it.name == "printInfoWithParam" }?.call(typeParameter, "p")
+    kClass.memberFunctions.filter { it.name == "printInfo" }.forEach { it.call(typeParameter) }
+    kClass.functions.find { it.name == "printInfo" }?.call(typeParameter)
+    kClass.functions.find { it.name == "printInfoWithParam" }?.call(typeParameter, "p")
 
-    val kProperty = TypeParameter::class.memberProperties.find { it.name == "k" }
+    val kProperty = kClass.memberProperties.find { it.name == "k" }
+    val vProperty = kClass.memberProperties.find { it.name == "v" }
     println(kProperty?.getter?.call(typeParameter))
     println(kProperty?.get(typeParameter))
     println(kProperty?.call(typeParameter))
@@ -43,4 +52,14 @@ fun main() {
         kProperty.setter.call(typeParameter, 3)
         println(typeParameter.k)
     }
+    println("------------------------------------")
+
+    val companionObject = kClass.companionObject
+    println(companionObject)
+    TypeParameter.companionMethod()
+    println("------------------------------------")
+
+    val createInstance = kClass.createInstance()
+    println("------------------------------------")
+    createInstance.printInfo()
 }
